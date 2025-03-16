1ab0ae2c0309b67d5f4a6fac873c7fef564f9393
callia <a.ostanin@meta.studio>
2025-03-16 18:09:21 +0700

    feat: add including custom news.list component

--- project/index.php
+++ project/index.php
@@ -1,8 +1,19 @@
-<?
+<?php
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('Главная');
-?> 
-
-<?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+$APPLICATION->SetTitle("Главная");
 ?>
+
+<?php
+$APPLICATION->IncludeComponent(
+	"custom:news.list", 
+	".default", 
+	array(
+		"ELEMENTS_PER_PAGE" => "2",
+		"IBLOCK_ID" => "1",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
+
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
